0f1fc388cdaaf57166c749a6774438aba4b31b1d
Diego <ale@xdasp.me>
2026-07-02 10:07:07 -0400

    xd

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,17 +1,17 @@
 data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.main.name
+  name = data.aws_eks_cluster.main.name
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.main.token
   }
 }
