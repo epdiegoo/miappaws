b6916e1791d3df8afe44d7a4c12b665a0b1e229e
Diego <ale@xdasp.me>
2026-07-02 10:10:27 -0400

    xd

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -1,4 +1,24 @@
-data "helm_release" "kube_prometheus_stack" {
-  name      = "kube-prometheus-stack"
-  namespace = "monitoring"
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  set {
+    name  = "grafana.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+    value = "false"
+  }
+
+  timeout = 900
+  wait    = true
+
+  depends_on = [
+    data.aws_eks_node_group.main
+  ]
 }
